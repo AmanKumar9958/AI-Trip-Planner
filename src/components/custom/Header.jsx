e969626c1313791a7eccf66dd58232906ae55466
Aman Kumar <amancollege04@gmail.com>
2025-03-11 22:52:09 +0530

    Add routing to Header component for logo and signup button

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Button } from '../ui/button'
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div className='flex items-center justify-between px-8 py-4'>
-            <img src="./Logo.svg" alt="Trip Planner" />
-            <Button>SignUp</Button>
+            <Link to={'/'}>
+                <img src="./Logo.svg" alt="Trip Planner" />
+            </Link>
+            <Link to={'/signup'}>
+                <Button>SignUp</Button>
+            </Link>
         </div>
     )
 }
