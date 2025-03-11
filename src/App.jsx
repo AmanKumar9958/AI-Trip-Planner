df5d85bfd9c11e5e56de17dc85d1260db541e254
Aman Kumar <amancollege04@gmail.com>
2025-03-11 22:53:23 +0530

    Add Header component to App and update CSS for button cursor

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Hero from './components/custom/Hero'
+import Header from './components/custom/Header'
 
 const App = () => {
   return (
     <div className=''>
+      <Header />
       <Hero />
     </div>
   )
