36848bed104edf60efe0c23812b2a2805200711d
Aman Kumar <amancollege04@gmail.com>
2025-03-11 22:52:19 +0530

    Add navigation link to home page in Login component

--- src/Page/Login.jsx
+++ src/Page/Login.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Button } from '../components/ui/button'
+import { Link } from 'react-router'
 
 const App = () => {
     return (
         <div className='text-red-400 bg-black w-full min-h-screen flex flex-col gap-5 justify-center items-center text-7xl font-bold'>
             Please Login
             <Button className={'cursor-pointer'}>Click Me!! </Button>
+            <Link to={'/'}>
+                <Button className={'cursor-pointer'}>Go to home page</Button>
+            </Link>
         </div>
     )
 }
