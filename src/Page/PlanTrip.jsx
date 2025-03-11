063105f37903b814052d1059cdceef48c7c27c03
Aman Kumar <amancollege04@gmail.com>
2025-03-11 22:53:12 +0530

    Add PlanTrip page with Header component

---
+++ src/Page/PlanTrip.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../components/custom/Header'
+
+const PlanTrip = () => {
+    return (
+        <div>
+            <Header />
+            <h1>Plan Your Next Journey</h1>
+        </div>
+    )
+}
+
+export default PlanTrip
